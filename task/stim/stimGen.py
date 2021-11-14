f13df24a5068ce214a37cd6f7c08196eada3b69c
seandamiandevine <seandamiandevine@gmail.com>
2021-11-14 18:01:52 -0500

    stimGen

--- task/stim/stimGen.py
+++ task/stim/stimGen.py
@@ -15,40 +15,51 @@ def checkDivis(Ncued, Neff=4, availableSamples=220):
             return True
     return False
 
-#for i in range(0, 220):
-#    Ncued  = i
-#    Nnocue = Ncued//3
-#    print('Do {} cued trials and {} not cued trials work? {}'.format(Ncued, Nnocue, checkDivis(Ncued)))
+for i in range(0, 220):
+    Ncued  = i
+    Nnocue = Ncued//3
+    print('Do {} cued trials and {} not cued trials work? {}'.format(Ncued, Nnocue, checkDivis(Ncued)))
 
-Ncued  = 144
-Nnocue = Ncued//3
-print('Total number of trials = {}'.format(Ncued+Nnocue))
+def stimGen(Ncued, verbose=True):
+    Nnocue = Ncued//3
+    works  = checkDivis(Ncued)    
+    if not works: 
+        raise ValueError('Number of cued trials must be divisibe by 12')
+        
+    print('Total number of trials = {}'.format(Ncued+Nnocue))
+        
+    # Load all stim data
+    stim    = pd.read_csv('all_test.csv')
+    effLevs = pd.unique(stim['Effort Code'])
+    
+    # Split not cued trials
+    nEach = Nnocue//len(effLevs)
+    
+    dfs = []
+    for level in effLevs:
+        tmp = stim[stim['Effort Code']==level]
+        tmp = tmp.sample(n=nEach)
+        dfs.append(tmp)
+        stim = stim[~stim.index.isin(tmp.index)]
+    nocued = pd.concat(dfs).reset_index(drop=True)
+    nocued.to_csv('nocued_{}.csv'.format(Nnocue))
+    
+    print('{} trials of each effort level in non-cued cond.'.format(nEach))
+    
+    # Split cued trials
+    nEach = Ncued//len(effLevs)
+    
+    dfs = []
+    for level in effLevs:
+        tmp = stim[stim['Effort Code']==level]
+        tmp = tmp.sample(n=nEach)
+        dfs.append(tmp)
+        stim = stim[~stim.index.isin(tmp.index)]
+       
+    cued = pd.concat(dfs).reset_index(drop=True)
+    cued.to_csv('cued_{}.csv'.format(Ncued))
+    
+    print('{} trials of each effort level in cued cond.'.format(nEach))
 
-# Load all stim data
-stim    = pd.read_csv('all_test.csv')
-effLevs = pd.unique(stim['Effort Code'])
-
-# Split not cued trials
-nEach = Nnocue//len(effLevs)
-
-dfs = []
-for level in effLevs:
-    tmp = stim[stim['Effort Code']==level]
-    tmp = tmp.sample(n=nEach)
-    dfs.append(tmp)
-    stim = stim[~stim.index.isin(tmp.index)]
-nocued = pd.concat(dfs).reset_index(drop=True)
-nocued.to_csv('nocued_{}.csv'.format(Nnocue))
-
-# Split cued trials
-nEach = Ncued//len(effLevs)
-
-dfs = []
-for level in effLevs:
-    tmp = stim[stim['Effort Code']==level]
-    tmp = tmp.sample(n=nEach)
-    dfs.append(tmp)
-    stim = stim[~stim.index.isin(tmp.index)]
-   
-cued = pd.concat(dfs).reset_index(drop=True)
-cued.to_csv('cued_{}.csv'.format(Ncued))
+Ncued_proposed = int(input('How many cued trials do you want? '))
+stimGen(Ncued_proposed)
